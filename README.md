99d815adfbc5cc4ad385dd765a6192f85e54179f
Maciej 'vesim' Kuliński <vesim809@pm.me>
2023-09-25 22:11:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# umm-zig - Allocator for targets with limited amount of RAM
+
+This is an allocator heavily inspired by umm_malloc, which is designed for ARM7 MCUs, 
+but it should work well for any 32/16/8-bits MCUs.
---
+++ build.zig
@@ -0,0 +1,23 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const lib_module = b.createModule(.{
+        .source_file = .{
+            .path = "src/lib.zig",
+        },
+    });
+    try b.modules.put(b.dupe("umm"), lib_module);
+
+    const lib_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/lib.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_lib_tests = b.addRunArtifact(lib_tests);
+    const test_step = b.step("test", "Run library tests");
+    test_step.dependOn(&run_lib_tests.step);
+}
