28b1b80ef7fdd1c313963371816df82f11508fbf
Tobias Kohlbau <tobias@kohlbau.de>
2024-01-27 12:29:13 +0100

    migrate to new build
    Use new fields from zig master.
    
    Signed-off-by: Tobias Kohlbau <tobias@kohlbau.de>

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
 
     const lib_module = b.createModule(.{
-        .source_file = .{
+        .root_source_file = .{
             .path = "src/lib.zig",
         },
     });
